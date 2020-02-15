2b23d45e8a85902677d0803cf4c18d67b1441c7e
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-15 19:41:27 +0100

    first attempt at a resource converter

---
+++ res/sprites/sssf_converter.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <fileio.h>
+
+typedef struct{
+    uint32_t colors;
+    uint32_t * palette;
+    uint16_t width, height;
+    uint8_t * byte_map;
+}sssf;
+
+sssf image;
+FILE *fp;
+
+int main(int argc, char **argv){
+    if(argc != 2){
+        printf("You must specify only a file\r\n");
+        return 1;
+    }
+
+    fp = fopen(argv[0],"r");
+
+    FILE * cursor = fp;
+
+    fread(&image.colors, sizeof(uint32_), 1, cursor);
+    fseek(cursor, 12, SEEK_SET);
+
+}
