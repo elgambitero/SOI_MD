e87df3b0e9527568c29e18b8f4d8291f3dba3325
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-16 20:52:58 +0100

    First version of the sssf_converter that is able to read the frame offsets

--- res/sprites/ripping/sssf_converter.c
+++ res/sprites/ripping/sssf_converter.c
@@ -1,25 +1,87 @@
 #include <stdio.h>
-#include <fileio.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define FILENAME_BUF 256
 
 typedef struct{
     uint32_t frames;
     uint32_t * frame_offset;
-}sssf_header;
+}Sssf_Header;
 
-sssf_header header;
-FILE *fp;
+typedef struct{
+    uint16_t width;
+    uint16_t height;
+    uint32_t h_length;
+    uint8_t * pixel_skip;
+    uint8_t * pixel;
+}SOI_Frame;
+
+Sssf_Header header;
+FILE *fin;
+FILE *fpalette;
+FILE *fout;
+
+char filename[FILENAME_BUF];
+
+uint16_t swap_endian_16(uint16_t num);
+uint32_t swap_endian_32(uint32_t num);
 
 int main(int argc, char **argv){
-    if(argc != 2){
-        printf("You must specify only a file\r\n");
+    if(argc != 4){
+        printf("You must specify an input file, a converted clut palette file, and a destination folder.\r\n");
         return 1;
     }
 
-    fp = fopen(argv[0],"r");
+    fin = fopen(argv[1],"r");
 
-    FILE * cursor = fp;
+    fread(&header.frames, sizeof(uint32_t), 1, fin);
+    fseek(fin, 8, SEEK_CUR);
 
-    fread(&header.frames, sizeof(uint32_), 1, cursor);
-    fseek(cursor, 12, SEEK_SET);
+    header.frames = swap_endian_32(header.frames);
 
+    printf("There are %d frames:\r\n",header.frames);
+
+    header.frame_offset = (uint32_t*) malloc(sizeof(uint32_t) * header.frames);
+
+    if(header.frame_offset == NULL) {printf("FRAME OFFSET MALLOC ERROR\r\n"); return 1;}
+
+    for(uint32_t i = 0; i < header.frames; i++){
+        fread(header.frame_offset + i, sizeof(uint32_t), 1, fin);
+        *(header.frame_offset + i) = swap_endian_32(*(header.frame_offset + i));
+    }
+
+
+    for(uint32_t i = 0; i < header.frames; i++){
+        printf("%04X, ",*(header.frame_offset + i));
+        if(i%8 == 0) printf("\r\n");
+    }
+
+    printf("\r\n");
+
+    fclose(fin);
+    free(header.frame_offset);
+
+}
+
+uint16_t swap_endian_16(uint16_t num){
+    uint16_t _num = num;
+    uint16_t result;
+    uint8_t *inptr = &_num;
+    uint8_t *outptr = &result;
+    for(int i = 0; i < 2; i++){
+        *(outptr + i) = *(inptr + (1 - i));
+    }
+    return result;
+}
+
+uint32_t swap_endian_32(uint32_t num){
+    uint32_t _num = num;
+    uint32_t result;
+    uint8_t *inptr = &_num;
+    uint8_t *outptr = &result;
+    for(int i = 0; i < 4; i++){
+        *(outptr + i) = *(inptr + (3 - i));
+    }
+    return result;
 }
