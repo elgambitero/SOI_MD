a5c395667e11cbdda63a308a8f7f945752670b7f
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-22 12:26:24 +0100

    Fix Animation flags in actors.h. Add Beenies' up and down actions

--- src/actors.h
+++ src/actors.h
@@ -27,14 +27,20 @@ u8 addActor(Actor * actor);
 u8 removeActor(Actor * actor);
 u8 updateActors();
 
-#define STILL_LEFT 0
-#define STILL_RIGHT 1
-#define WALK_RIGHT 1
-#define WALK_LEFT 2
-#define RIGHT_TURN_LEFT 3
-#define LEFT_TURN_RIGHT 4
-#define ATTACK_LEFT 5
-#define ATTACK_RIGHT 6
+#define STILL_RIGHT 0
+#define STILL_LEFT 1
+#define WALK_RIGHT 2
+#define WALK_LEFT 3
+#define RIGHT_TURN_LEFT 4
+#define LEFT_TURN_RIGHT 5
+#define ATTACK_LEFT 6
+#define ATTACK_RIGHT 7
+#define WALK_UP 8
+#define WALK_DOWN 9
+#define UP_TURN_DOWN 10
+#define DOWN_TURN_UP 11
+#define ATTACK_UP 12
+#define ATTACK_DOWN 13
 
 #define SLOW 4
 #define NORMAL 2
