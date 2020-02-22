1ea613f9c2b9de7253cc0df3132fd38481c4818d
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-22 18:04:52 +0100

    Expand attributes field to 16 bit

--- src/entities.h
+++ src/entities.h
@@ -8,33 +8,48 @@
 #define SIZE_X(size) (size && B00001111)
 #define SIZE_Y(size) (size >> 4)
 
-
 typedef struct Entity{
-    u8 attr;
+    u16 attr;
     u8 size;
     Palette * palette;
     SpriteDefinition * sprite_def;
 }Entity;
 
-#define PLAYER       0
-#define BLUE_PLAYER  B00000000
-#define GREEN_PLAYER B00000100
+#define ENT_CHECK_BITMSK B0000000000000011
 
-#define NASTIE       1
-#define BREAKS       B00000100
-#define BREAKS_THRU  B00001100
-#define LEAPS        B00010000
-#define DIES_ON_LEAP B00110000
-#define LURKS        B00100000
-#define IS_FAST      B01000000
-#define SHOOTS       B10000000
-#define INMOVILE     B00101000
-#define IS_BLOCK     B00111000
+#define BIG_ENTITY   0
 
-#define WHICH_NASTIE(attr) (attr >> 4)
+#define BLUE_PLAYER   B0000000000000000
+#define GREEN_PLAYER  B0000000000000100
+#define KNIGHT        B0000000000001100
+#define GARGOYLE      B0000000000001000
+
+#define NASTIE        1
+
+#define MOVT_BITMSK   B0000000000001100
+
+#define STILL         B0000000000000000
+#define WALKS         B0000000000000100
+#define FLIES         B0000000000001100
+#define LURKS         B0000000000001000
+
+#define BRK_BITMSK    B0000000000110000
+
+#define BREAKS        B0000000000010000
+#define BREAKS_THRU   B0000000000110000
+
+#define LEAP_BITMSK   B0000000011000000
+
+#define LEAPS         B0000000001000000
+#define DIES_ON_LEAP  B0000000011000000
+#define TURNS_ON_LEAP B0000000010000000
+
+#define CENTERED      B0000000100000000
+#define SHOOTS        B0000001000000000
 
 #define PROJECTILE     2
-#define CREATES_NASTIE B00001000
+#define CREATES_NASTIE B0000000100000000
+#define WHICH_NASTIE(attr) (attr >> 9)
 
 
 #endif
