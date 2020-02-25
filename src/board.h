4498963f85ab9ac84aeebe4b70f95eb59083e95c
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-25 19:30:07 +0100

    Make blocks a 16bit number. Add some helpful defines

--- src/board.h
+++ src/board.h
@@ -6,11 +6,15 @@
 
 #define BOARD_X 19
 #define BOARD_Y 13
-#define BOARD_BUFFER BOARD_X*BOARD_Y
+#define BOARD_BUFFER BOARD_X * BOARD_Y
+
+#define BOARD_X_PX BOARD_X * 16
+#define BOARD_Y_PX BOARD_Y * 16
+
 
 typedef struct Board{
-    u8 front_blocks[BOARD_BUFFER];
-    u8 back_blocks[BOARD_BUFFER];
+    u16 front_blocks[BOARD_BUFFER];
+    u16 back_blocks[BOARD_BUFFER];
     Actor * actors;
 } Board;
 
