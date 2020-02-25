de8dcd4c75bfec8aa297ff6085389b6e7cb9931c
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-02-25 18:57:58 +0100

    Use memcpy in board again

--- src/board.c
+++ src/board.c
@@ -1,12 +1,14 @@
 #include "board.h"
 
 u8 load_board(Board * board, const Board * level){
+    /*
     for(u8 i = 0; i < BOARD_BUFFER; i++){
         board->front_blocks[i] = level->front_blocks[i];
         board->back_blocks[i] = level->back_blocks[i];
     }
     board->actors = level->actors;
-    //memcpy(board, level, sizeof(Board));
+    */
+    memcpy(board, level, sizeof(Board));
     Actor * actor = board->actors;
     while(actor){
         ACT_add(actor);
