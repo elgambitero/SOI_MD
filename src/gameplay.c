0a76542170db2646b7735ec387c0ad6abf368ed5
Jaime García Villena <garciavillena.jaime@gmail.com>
2020-03-05 23:45:59 +0100

    Draw even more blocks!

--- src/gameplay.c
+++ src/gameplay.c
@@ -73,6 +73,8 @@ void gameInit(){
             1, 1, 0, 0, 12, 2);
     VDP_setMapEx(PLAN_B, clr_blk_sl1.map, TILE_ATTR_FULL(PAL2, FALSE, FALSE, FALSE, clr_blk_sl1_ind),
             1, 1, 0, 0, 12, 2);
+    VDP_setMapEx(PLAN_A, wt_blk.map, TILE_ATTR_FULL(PAL2, FALSE, FALSE, FALSE, wt_blk_ind),
+            1, 3, 0, 0, 12, 2);
 
     VDP_setMapEx(PLAN_B, sp_blk.map, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, sp_blk_ind),
             13, 1, 0, 0, 12, 2);
