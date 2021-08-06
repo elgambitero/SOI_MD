5da4021655b2bfc378323ccbffa8c0de19900830
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2021-08-06 17:57:48 +0200

    Reorder block macros into bitshifts

--- inc/blocks.h
+++ inc/blocks.h
@@ -2,107 +2,115 @@
 
 #define BLOCKS_H
 
-#define BLK_TYPE        (3 << 0)
+#define BLK_TYPE        (3 << 0) //2 bits
 
 #define NORMAL_BLOCK    0x0001
 
-#define SOLID           (1 << 2)
-#define BREAKABLE       (1 << 3)
-#define BROKEN          (1 << 4)
+#define SOLID                     (1 << 2)
+#define BREAKABLE                 (1 << 3)
+#define BROKEN                    (1 << 4)
 
-#define NBLK_TYP_MSK   (7 << 5) //0x00E0
+#define NBLK_TYP_SHFT          5
 
-#define BLK_NOTHING    (0 << 5)
-#define BLK_FLAT       (1 << 5)
-#define BLK_DOT        (2 << 5)
-#define BLK_DONUT      (3 << 5)
-#define BLK_CHISEL     (4 << 5)
-#define BLK_NET        (5 << 5)
-#define BLK_QUESTION   (6 << 5)
-#define BLK_PLAYER     (7 << 5)
+#define NBLK_TYP_MSK              (7 << NBLK_TYP_SHFT) //3 bits
 
-#define NBLK_COL_MSK   (3 << 8)
+#define BLK_NOTHING               (0 << NBLK_TYP_SHFT)
+#define BLK_FLAT                  (1 << NBLK_TYP_SHFT)
+#define BLK_DOT                   (2 << NBLK_TYP_SHFT)
+#define BLK_DONUT                 (3 << NBLK_TYP_SHFT)
+#define BLK_CHISEL                (4 << NBLK_TYP_SHFT)
+#define BLK_NET                   (5 << NBLK_TYP_SHFT)
+#define BLK_QUESTION              (6 << NBLK_TYP_SHFT)
+#define BLK_PLAYER                (7 << NBLK_TYP_SHFT)
 
-#define GET_BLK_PAL(byte) (byte >> 8)
+#define NBLK_COL_SHFT          8
 
-#define COL_WHITE     (0 << 8)
-#define COL_SLOT1     (1 << 8)
-#define COL_SLOT2     (2 << 8)
-#define COL_SLOT3     (3 << 8)
-
-#define PL_BLUE       (0 << 8)
-#define PL_GREEN      (1 << 8)
-
-#define PAL_GR 0
-#define PAL_R  1
-#define PAL_BL 2
-#define PAL_PI 3
-#define PAL_YL 4
-#define PAL_BK 5
+#define NBLK_COL_MSK              (3 << NBLK_COL_SHFT)
 
 
-#define SPECIAL_BLOCK   (1 << 1) //0x0002
+#define COL_WHITE                 (0 << NBLK_COL_SHFT)
+#define COL_SLOT1                 (1 << NBLK_COL_SHFT)
+#define COL_SLOT2                 (2 << NBLK_COL_SHFT)
+#define COL_SLOT3                 (3 << NBLK_COL_SHFT)
 
-#define SP_ACTIVE       (1 << 2) //0x0004
+#define PL_BLUE                   (0 << NBLK_COL_SHFT)
+#define PL_GREEN                  (1 << NBLK_COL_SHFT)
 
-#define SP_TYP_MSK      (3 << 5) //0x0060
-
-#define SP_TRANS        (1 << 5)
-#define SP_SWITCH       (2 << 5)
-#define SP_FLOP         (3 << 5)
-
-#define TRANS_DIR_MSK   (1 << 11) //0x0800
-
-#define TRANS_IN        (1 << 11) //0x0800
-#define TRANS_OUT       0x0000
+#define PAL_GR                    0
+#define PAL_R                     1
+#define PAL_BL                    2
+#define PAL_PI                    3
+#define PAL_YL                    4
+#define PAL_BK                    5
 
 
-#define SP_COL_MSK      (7 << 8) //0x0700
+#define SPECIAL_BLOCK           0x0002
 
-#define SP_COL_GR       (0 << 8)
-#define SP_COL_R        (1 << 8)
-#define SP_COL_BL       (2 << 8)
-#define SP_COL_PI       (3 << 8)
-#define SP_COL_YL       (4 << 8)
-#define SP_COL_WT       (5 << 8)
+#define SP_ACT_SHFT             2
+#define SP_ACTIVE              (1 << SP_ACT_SHFT) //1 bit
 
-#define GOODIE          (3 << 0) //0x0003
-#define GOOD_TYP_MSK    (0x3F << 4) //0x03F0
-#define GD_GET_INDEX(good) ( ( GOOD_TYP_MSK & good) >> 4 )
+#define SP_TYP_SHFT             5
 
-#define GD_ADD_INFO(good) ( good << 8 )
-#define GD_GET_INFO(gd)   ( gd >> 8 )
+#define SP_TYP_MSK             (3 << SP_TYP_SHFT) //2 bit
 
-#define GDi_SILVC         (0 << 4)
-#define GDi_ARROW         (1 << 4)
-#define GDi_CLK3h         (2 << 4)
-#define GDi_CLK6h         (3 << 4)
-#define GDi_CLK9h         (4 << 4)
-#define GDi_CLK12h        (5 << 4)
-#define GDi_GLASS         (6 << 4)
-#define GDi_SHLD          (7 << 4)
-#define GDi_BOOT          (8 << 4)
-#define GDi_THNBALL       (9 << 4)
-#define GDi_RDKEY         (10 << 4)
-#define GDi_BLKEY         (11 << 4)
-#define GDi_RDDOOR        (12 << 4)
-#define GDi_BLDOOR        (13 << 4)
-#define GDi_RDDOOR_open   (14 << 4)
-#define GDi_BLDOOR_open   (15 << 4)
-#define GDi_GOLDC         (16 << 4)
-#define GDi_1UP           (17 << 4)
-#define GDi_2x            (18 << 4)
-#define GDi_3x            (19 << 4)
-#define GDi_4x            (20 << 4)
-#define GDi_5x            (21 << 4)
-#define GDi_BALL          (22 << 4)
-#define GDi_GEM           (23 << 4)
-#define GDi_GOLD          (24 << 4)
-#define GDi_TURTL         (25 << 4)
-#define GDi_STAR          (26 << 4)
-#define GDi_GRKEY         (27 << 4)
-#define GDi_GRDOOR        (28 << 4)
-#define GDi_GRDOOR_open   (29 << 4)
+#define SP_TRANS               (1 << SP_TYP_SHFT)
+#define SP_SWITCH              (2 << SP_TYP_SHFT)
+#define SP_FLOP                (3 << SP_TYP_SHFT)
+
+#define TRANS_DIR_SHFT          11
+
+#define TRANS_DIR_MSK          (1 << TRANS_DIR_SHFT) //1bit
+
+#define TRANS_IN               (1 << TRANS_DIR_SHFT) //0x0800
+#define TRANS_OUT              (0 << TRANS_DIR_SHFT) 
+
+#define SP_COL_SHFT             8
+
+#define SP_COL_MSK             (7 << SP_COL_SHFT) //3bit
+
+#define SP_COL_GR              (0 << SP_COL_SHFT)
+#define SP_COL_R               (1 << SP_COL_SHFT)
+#define SP_COL_BL              (2 << SP_COL_SHFT)
+#define SP_COL_PI              (3 << SP_COL_SHFT)
+#define SP_COL_YL              (4 << SP_COL_SHFT)
+#define SP_COL_WT              (5 << SP_COL_SHFT)
+
+#define GOODIE                  0x0003
+
+#define GOOD_TYP_SHFT           4
+#define GOOD_TYP_MSK           (0x3F << GOOD_TYP_SHFT) //5bit
+#define GD_GET_INDEX(good)     ( ( GOOD_TYP_MSK & good) >> GOOD_TYP_SHFT )
+
+#define GDi_SILVC              (0 << GOOD_TYP_SHFT)
+#define GDi_ARROW              (1 << GOOD_TYP_SHFT)
+#define GDi_CLK3h              (2 << GOOD_TYP_SHFT)
+#define GDi_CLK6h              (3 << GOOD_TYP_SHFT)
+#define GDi_CLK9h              (4 << GOOD_TYP_SHFT)
+#define GDi_CLK12h             (5 << GOOD_TYP_SHFT)
+#define GDi_GLASS              (6 << GOOD_TYP_SHFT)
+#define GDi_SHLD               (7 << GOOD_TYP_SHFT)
+#define GDi_BOOT               (8 << GOOD_TYP_SHFT)
+#define GDi_THNBALL            (9 << GOOD_TYP_SHFT)
+#define GDi_RDKEY              (10 << GOOD_TYP_SHFT)
+#define GDi_BLKEY              (11 << GOOD_TYP_SHFT)
+#define GDi_RDDOOR             (12 << GOOD_TYP_SHFT)
+#define GDi_BLDOOR             (13 << GOOD_TYP_SHFT)
+#define GDi_RDDOOR_open        (14 << GOOD_TYP_SHFT)
+#define GDi_BLDOOR_open        (15 << GOOD_TYP_SHFT)
+#define GDi_GOLDC              (16 << GOOD_TYP_SHFT)
+#define GDi_1UP                (17 << GOOD_TYP_SHFT)
+#define GDi_2x                 (18 << GOOD_TYP_SHFT)
+#define GDi_3x                 (19 << GOOD_TYP_SHFT)
+#define GDi_4x                 (20 << GOOD_TYP_SHFT)
+#define GDi_5x                 (21 << GOOD_TYP_SHFT)
+#define GDi_BALL               (22 << GOOD_TYP_SHFT)
+#define GDi_GEM                (23 << GOOD_TYP_SHFT)
+#define GDi_GOLD               (24 << GOOD_TYP_SHFT)
+#define GDi_TURTL              (25 << GOOD_TYP_SHFT)
+#define GDi_STAR               (26 << GOOD_TYP_SHFT)
+#define GDi_GRKEY              (27 << GOOD_TYP_SHFT)
+#define GDi_GRDOOR             (28 << GOOD_TYP_SHFT)
+#define GDi_GRDOOR_open        (29 << GOOD_TYP_SHFT)
 
 #define  BP ( NORMAL_BLOCK | SOLID | BREAKABLE | BLK_PLAYER | PL_BLUE )
 #define  GP ( NORMAL_BLOCK | SOLID | BREAKABLE | BLK_PLAYER | PL_GREEN )
