a9e74476aafc1a147ece81be9f040d7ebb916313
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2021-08-16 23:04:53 +0200

    Lay out level 2

--- res/level2.c
+++ res/level2.c
@@ -6,13 +6,17 @@
 #define BOW (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_WHITE)
 #define BFW (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_WHITE)
 #define BFR (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_SLOT1)
+#define BFG (NORMAL_BLOCK | SOLID | BREAKABLE | BLK_FLAT  | COL_SLOT2)
 #define RDK (GOODIE | GDi_RDKEY)
 #define RDD (GOODIE | GDi_RDDOOR)
+#define SLC (GOODIE | GDi_SILVC)
+#define GLC (GOODIE | GDi_GOLDC)
+#define C3H (GOODIE | GDi_CLK3h)
 
 static const Actor blp = {&PL_blue,
        0,
        0,
-    {120, 112},
+    {BLOCK_TO_PX(4) - 8, BLOCK_TO_PX(13) + 16},
     {   0,    0},
       0,
       0,
@@ -22,51 +26,40 @@ static const Actor blp = {&PL_blue,
    static const Actor glp = {&PL_green,
       0,
       0,
-   {184, 112},
+   {BLOCK_TO_PX(5) - 8, BLOCK_TO_PX(13) + 16},
    {   0,    0},
       0,
       0,
       &blp
    };
 
-static const Actor robo = {&NST_robo,
-   WALK_RIGHT,
-   0,
-   {40, 32},
-   {0, 0},
-      0,
-   0,
-   0,
-   };
-
-
 
 const Board level2 = {
     {
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0, BFG, BFG, SLC, BFG, BFG, BFG,   0, SLC, BFG, BFG, C3H,   0,   0,
+       0,   0, BFG, BFG, BFG, BFG, GLC, BFG, BFG, BFG, BFG, BFG, BFG, BFG,   0,   0, BFG, BFG,   0,
+       0, BFG, BFG, BFG, BFG, BFW, BFW, BFW, BFW, GLC, BFG, BFG, BFG, BFG, BFW, BFG, BFG, BFG,   0,
+       0, GLC, BFG, BFG, SLC,   0, BFG, BFG, BFW, BFG, BFG,   0, BFW, BFW, BFG, BFG, BFG, BFG,   0,
+       0, BFG, BFG, BFG, BFG, BFG, GLC,   0, BFW, BFW, RDK, BFW, BFW, BFW, BFG, GLC, BFG, BFG,   0,
+       0, BFG, BFG, BFG, BFW, BFW, BFG,   0, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFG, BFG,   0,   0,
+       0, BFG, BFG, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFG,   0, BFG,   0,   0,   0,
+       0,   0, BFG,   0, BFG, BFG,   0, BFW, BFW, BFW, BFW, BFW,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0, BFG,   0,   0,   0,   0, BFW, BFW, BFW,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0, BFW, BFW, BFW,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0, BFW, BFW, BFW,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0, BFW, BFW, BFW, BFW, BFW,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0, BFW, BFW, BFW, BFW, BFW, BFW, RDD,   0,   0,   0,   0,   0,
     },
 
     {
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0, BFW,   0,   0,   0, BFW,   0,   0,   0, BFW,   0,   0,   0,   0, BFW,   0,   0,
+       0,   0,   0,   0, BFW,   0,   0,   0,   0,   0,   0, BFW, BFW,   0,   0, BFW,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW, BFW, BFW,   0,   0,
+       0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0,   0,   0,
+       0,   0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
@@ -76,7 +69,7 @@ const Board level2 = {
     {PAL_R, PAL_GR, PAL_BL},
     MUS_HEAVY1, //music
     0,
-    5000, //bonus
+    6000, //bonus
     NULL, //nasties
     &glp //players
 };
