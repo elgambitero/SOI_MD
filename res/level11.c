0d3845c99d84461f81b2663f3ffdcd2d6e4a35f7
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2021-08-17 01:28:47 +0200

    Lay out level 11

--- res/level11.c
+++ res/level11.c
@@ -2,17 +2,33 @@
 
 #define BDR (NORMAL_BLOCK | SOLID | BLK_DONUT | COL_SLOT1)
 #define BOR (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_SLOT1)
-#define BDW (NORMAL_BLOCK | SOLID | BLK_DONUT | COL_WHITE)
+#define BDW (NORMAL_BLOCK | SOLID | BREAKABLE |  BLK_DONUT | COL_WHITE)
 #define BOW (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_WHITE)
-#define BFW (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_WHITE)
+#define BFW (NORMAL_BLOCK | SOLID | BREAKABLE | BLK_FLAT  | COL_WHITE)
 #define BFR (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_SLOT1)
+#define NET (NORMAL_BLOCK | BLK_NET)
 #define RDK (GOODIE | GDi_RDKEY)
 #define RDD (GOODIE | GDi_RDDOOR)
+#define RDK (GOODIE | GDi_RDKEY)
+#define RDD (GOODIE | GDi_RDDOOR)
+#define SLC (GOODIE | GDi_SILVC)
+#define GLC (GOODIE | GDi_GOLDC)
+#define ARR (GOODIE | GDi_ARROW)
+#define C3H (GOODIE | GDi_CLK3h)
+#define GLS (GOODIE | GDi_GLASS)
+#define M2X (GOODIE | GDi_2x )
+#define M3X (GOODIE | GDi_3x )
+#define THN (GOODIE | GDi_THNBALL)
+#define GEM (GOODIE | GDi_GEM)
+#define TUR (GOODIE | GDi_TURTL)
+#define SHL (GOODIE | GDi_SHLD)
+#define BOO (GOODIE | GDi_BOOT)
+#define UP1 (GOODIE | GDi_1UP)
 
 static const Actor blp = {&PL_blue,
        0,
        0,
-    {120, 112},
+    {BLOCK_TO_PX(1) - 8, BLOCK_TO_PX(13)},
     {   0,    0},
       0,
       0,
@@ -22,40 +38,158 @@ static const Actor blp = {&PL_blue,
    static const Actor glp = {&PL_green,
       0,
       0,
-   {184, 112},
+   {BLOCK_TO_PX(2) - 8, BLOCK_TO_PX(13)},
    {   0,    0},
       0,
       0,
       &blp
    };
 
-static const Actor robo = {&NST_robo,
-   WALK_RIGHT,
+static const Actor bean = {&NST_beanie,
+   WALK_LEFT,
    0,
-   {40, 32},
-   {0, 0},
+   {BLOCK_TO_PX(19) - 8, BLOCK_TO_PX(2)},
+   {-NASTIE_SPEED, 0},
       0,
    0,
    0,
    };
 
+static const Actor spinner0 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(1) - 8, BLOCK_TO_PX(8)},
+   {0, 0},
+      0,
+   0,
+   &bean,
+   };
 
+static const Actor spinner1 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(2) - 8, BLOCK_TO_PX(6)},
+   {0, 0},
+      0,
+   0,
+   &spinner0,
+   };
+
+static const Actor spinner2 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(3) - 8, BLOCK_TO_PX(8)},
+   {0, 0},
+      0,
+   0,
+   &spinner1,
+};
+
+static const Actor spinner3 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(4) - 8, BLOCK_TO_PX(6)},
+   {0, 0},
+      0,
+   0,
+   &spinner2,
+};
+
+static const Actor spinner4 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(6) - 8, BLOCK_TO_PX(9)},
+   {0, 0},
+      0,
+   0,
+   &spinner3,
+};
+
+static const Actor spinner5 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(9) - 8, BLOCK_TO_PX(6)},
+   {0, 0},
+      0,
+   0,
+   &spinner4,
+};
+
+static const Actor spinner6 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(11) - 8, BLOCK_TO_PX(10)},
+   {0, 0},
+      0,
+   0,
+   &spinner5,
+};
+
+static const Actor spinner7 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(15) - 8, BLOCK_TO_PX(5)},
+   {0, 0},
+      0,
+   0,
+   &spinner6,
+};
+
+static const Actor spinner8 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(16) - 8, BLOCK_TO_PX(6)},
+   {0, 0},
+      0,
+   0,
+   &spinner7,
+};
+
+static const Actor spinner9 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(17) - 8, BLOCK_TO_PX(7)},
+   {0, 0},
+      0,
+   0,
+   &spinner8,
+};
+
+static const Actor spinner10 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(18) - 8, BLOCK_TO_PX(6)},
+   {0, 0},
+      0,
+   0,
+   &spinner9,
+};
+
+static const Actor spinner11 = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(19) - 8, BLOCK_TO_PX(5)},
+   {0, 0},
+      0,
+   0,
+   &spinner10,
+};
 
 const Board level11 = {
     {
+     SLC,   0,   0,   0,   0,   0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0, SLC,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0, BOW,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0, BOW,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0, BOW,   0, BOW, BOW,   0, BOW, BOW, BOW,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0, BOW,   0,   0,   0, SLC,   0,   0, SLC, BOW, BDW,   0,   0,   0, BDW,
+       0, BDW,   0, BDW, BOW, RDK, BOW, SLC, BOW, SLC, BOW,   0,   0, BOW,   0, BDW,   0, BDW,   0,
+       0,   0,   0,   0, BOW,   0,   0,   0, BOW,   0, BOW, NET, NET, BOW,   0,   0, BDW,   0,   0,
+     BDW,   0, BDW,   0, BOW,   0,   0,   0,   0,   0, BOW,   0,   0, BOW,   0,   0, SLC,   0,   0,
+       0,   0,   0,   0, BOW, BFW, BOW,   0, BOW, NET,   0,   0,   0, BOW,   0, SLC,   0, SLC,   0,
+       0,   0,   0,   0, BOW,   0, BOW,   0, BOW,   0, BOW,   0, BOW,   0, SLC,   0,   0,   0, SLC,
+       0,   0,   0,   0,   0,   0,   0,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0, RDD,
     },
 
     {
@@ -76,7 +210,7 @@ const Board level11 = {
     {PAL_R, PAL_GR, PAL_BL},
     MUS_HEAVY1, //music
     0,
-    5000, //bonus
-    NULL, //nasties
+    6000, //bonus
+    &spinner11, //nasties
     &glp //players
 };
