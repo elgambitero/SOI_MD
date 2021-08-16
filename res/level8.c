c06ed9bd46f2109f96326b5776f4c65b58f7cbed
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2021-08-17 00:33:23 +0200

    Lay out level 8. Switch players position in level 5

--- res/level8.c
+++ res/level8.c
@@ -8,11 +8,23 @@
 #define BFR (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_SLOT1)
 #define RDK (GOODIE | GDi_RDKEY)
 #define RDD (GOODIE | GDi_RDDOOR)
+#define RDK (GOODIE | GDi_RDKEY)
+#define RDD (GOODIE | GDi_RDDOOR)
+#define SLC (GOODIE | GDi_SILVC)
+#define GLC (GOODIE | GDi_GOLDC)
+#define ARR (GOODIE | GDi_ARROW)
+#define C3H (GOODIE | GDi_CLK3h)
+#define GLS (GOODIE | GDi_GLASS)
+#define M2X (GOODIE | GDi_2x )
+#define M3X (GOODIE | GDi_3x )
+#define THN (GOODIE | GDi_THNBALL)
+#define GEM (GOODIE | GDi_GEM)
+#define TUR (GOODIE | GDi_TURTL)
 
 static const Actor blp = {&PL_blue,
        0,
        0,
-    {120, 112},
+    {BLOCK_TO_PX(9) - 8, BLOCK_TO_PX(12)},
     {   0,    0},
       0,
       0,
@@ -22,61 +34,69 @@ static const Actor blp = {&PL_blue,
    static const Actor glp = {&PL_green,
       0,
       0,
-   {184, 112},
+   {BLOCK_TO_PX(11) - 8, BLOCK_TO_PX(12)},
    {   0,    0},
       0,
       0,
       &blp
    };
 
-static const Actor robo = {&NST_robo,
+static const Actor spawn = {&SPW_ostrich,
    WALK_RIGHT,
    0,
-   {40, 32},
+   {BLOCK_TO_PX(18) - 8, BLOCK_TO_PX(3)},
    {0, 0},
-      0,
+      MAX_TIMER - SPAWN_TIME,
    0,
    0,
    };
 
-
+static const Actor spawn1 = {&SPW_ant,
+   WALK_LEFT,
+   0,
+   {BLOCK_TO_PX(8) - 8, BLOCK_TO_PX(3)},
+   {0, 0},
+      MAX_TIMER - SPAWN_TIME,
+   0,
+   &spawn,
+   };
 
 const Board level8 = {
     {
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,
+     BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0,   0,   0,   0,   0, FRO, BFW,
+     BFW,   0,   0,   0,   0,   0,   0, ANT, BFW,   0, BFW,   0,   0,   0,   0,   0,   0, OST, BFW,
+     BFW,   0,   0,   0, BFW, BFW, BFW, BFW, BFW,   0, BFW,   0,   0,   0, BFW, BFW, BFW, BFW, BFW,
+     BFW,   0,   0,   0,   0,   0,   0,   0, BFW, RDK, BFW,   0,   0,   0,   0,   0,   0,   0, BFW,
+     BFW,   0,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0,   0, BFW,
+     BFW, BFW, BFW, BFW, BFW,   0,   0,   0, BFW,   0, BFW, BFW, BFW, BFW, BFW,   0,   0,   0, BFW,
+     BFW,   0,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0,   0, BFW,
+     BFW,   0,   0,   0,   0,   0,   0,   0, BFW, RDD, BFW,   0,   0,   0,   0,   0,   0,   0, BFW,
+     BFW,   0,   0, BFW, BFW, BFW, BFW, BFW, BFW, FBO, BFW,   0,   0,   0, BFW, BFW, BFW, BFW, BFW,
+     BFW,   0,   0,   0,   0,   0,   0, BFW,   0,   0,   0, BFW,   0,   0,   0,   0,   0,   0, BFW,
+     BFW,   0,   0,   0,   0, THN, GRO,   0,   0,   0,   0,   0, GBO, THN,   0,   0,   0,   0, BFW,
+     BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,
     },
 
     {
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0, ARR,   0,   0,   0,   0,   0,   0,   0,   0,   0, TUR,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0, GLS,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, SLC,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0, M2X,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
-       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, GLC,   0,
        0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
     },
     {PAL_R, PAL_GR, PAL_BL},
     MUS_HEAVY1, //music
     0,
-    5000, //bonus
-    NULL, //nasties
+    6000, //bonus
+    &spawn1, //nasties
     &glp //players
 };
