5dd8f473a8c4546d5ba3b5a4baaa0febf938d966
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2021-08-16 20:30:46 +0200

    Fill the res/ folder with empty levels to begin the porting

--- inc/levels.h
+++ inc/levels.h
@@ -8,6 +8,7 @@
 
 const Board test_level;
 const Board level1;
+const Board level2;
 const Board level6;
 
 extern const Board * levels[];
--- src/levels.c
+++ src/levels.c
@@ -3,7 +3,19 @@
 const Board * levels[] = {
     &test_level,
     &level1,
-    &level6
+    &level2,
+    &level2,
+    &level3,
+    &level4,
+    &level5,
+    &level6,
+    &level7,
+    &level8,
+    &level9,
+    &level10,
+    &level11,
+    &level12,
+    &level13
 };
 
-const u8 max_levels = 3; //Not dynamic. Should be calculated upon compiling.
+const u8 max_levels = 14; //Not dynamic. Should be calculated upon compiling.
