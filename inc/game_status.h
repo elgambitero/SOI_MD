013ebd36070f72d589b444b0bd02cd021f515abc
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2022-04-30 01:02:48 +0200

    Use levelset index rather than the pointer of SOI_game_status_t

--- inc/game_status.h
+++ inc/game_status.h
@@ -5,8 +5,8 @@
 
 typedef struct SOI_game_status_t{
     uint8_t current_level;
+    uint8_t level_set_ind;
     SOI_player_status_t player_stat[MAX_PLAYERS];
-    const SOI_level_set_t * level_set;
 }SOI_game_status_t;
 
 #endif
