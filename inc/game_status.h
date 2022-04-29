ab2a328a25f2a37e2ca2e9e3b738d28bd3f06ae8
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2022-04-29 22:11:45 +0200

    Include player status in game status

--- inc/game_status.h
+++ inc/game_status.h
@@ -1,8 +1,12 @@
 #ifndef GAME_STATUS_H
 #define GAME_STATUS_H
 
-typedef struct SOI_game_status_t{
+#include "player_status.h"
 
+#define MAX_PLAYERS 2
+
+typedef struct SOI_game_status_t{
+    SOI_player_status_t player_stat[MAX_PLAYERS];
 }SOI_game_status_t;
 
 #endif
