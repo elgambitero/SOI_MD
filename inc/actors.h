98430c46c4b66856ea0400876206084e43d91dc4
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2022-04-29 23:13:14 +0200

    Add fundamental SOI data structures

---
+++ inc/actors.h
@@ -0,0 +1,9 @@
+#ifndef ACTORS_H
+#define ACTORS_H
+
+typedef struct SOI_actor_t{
+
+    struct SOI_actor_t * next;
+}SOI_actor_t;
+
+#endif
---
+++ inc/board.h
@@ -0,0 +1,8 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+typedef struct{
+    
+}SOI_board_t;
+
+#endif
