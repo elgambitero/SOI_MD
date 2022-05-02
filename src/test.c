124250cb93a1210a11f5297afdbd18a0838c3812
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2022-05-02 18:49:08 +0200

    Reorder prints in test.c

--- src/test.c
+++ src/test.c
@@ -24,13 +24,13 @@ void TST_game_config(game_config_t config){
 
     u16 * seedPtr = (u16*) config.seed;
 
-    sprintf(text_buffer, "%05d", config.start_level);
+    sprintf(text_buffer, "%05u", config.start_level);
     VDP_drawText(text_buffer, GMCFG_INDENT, 1);
 
-    sprintf(text_buffer, "%05d", config.start_level_set);
+    sprintf(text_buffer, "%05u", config.start_level_set);
     VDP_drawText(text_buffer, GMCFG_INDENT, 2);
 
-    sprintf(text_buffer, "%05d", config.num_players);
+    sprintf(text_buffer, "%05u", config.num_players);
     VDP_drawText(text_buffer, GMCFG_INDENT, 3);
 
     sprintf(text_buffer, "%05u", *seedPtr);
@@ -65,13 +65,7 @@ void TST_game_status(SOI_game_status_t * status){
 void TST_print_game_status_header(){
     VDP_drawText("current_level: ", 0, 1);
     VDP_drawText("level_set_ind: ", 0, 2);
-    VDP_drawText("Player 1: ", 0, 3);
-    VDP_drawText("score: " , 0, 4);
-    VDP_drawText("bonus: " , 0, 5);
-    VDP_drawText("mult: " , 0, 6);
-    VDP_drawText("arrows: " , 0, 7);
-    VDP_drawText("balls: " , 0, 8);
-    VDP_drawText("lives: " , 0, 9);
+    VDP_drawText("Player stats: ", 0, 3);
 }
 
 void TST_print_player_game_status(
@@ -79,18 +73,32 @@ void TST_print_player_game_status(
     uint8_t player_index
 )
 {
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].score);
+
+    VDP_drawText("score: " , 0, 4 + player_index * 6);
+    VDP_drawText("bonus: " , 0, 5 + player_index * 6);
+    VDP_drawText("mult: " , 0, 6 + player_index * 6);
+    VDP_drawText("arrows: " , 0, 7 + player_index * 6);
+    VDP_drawText("balls: " , 0, 8 + player_index * 6);
+    VDP_drawText("lives: " , 0, 9 + player_index * 6);
+
+    sprintf(text_buffer, "%08u" , status->player_stat[player_index].score);
     VDP_drawText(text_buffer, GMSTS_INDENT, 4 + player_index * 6);
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].bonus);
+
+    sprintf(text_buffer, "%05u" , status->player_stat[player_index].bonus);
     VDP_drawText(text_buffer, GMSTS_INDENT, 5 + player_index * 6);
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].mult);
+
+    sprintf(text_buffer, "%05u" , status->player_stat[player_index].mult);
     VDP_drawText(text_buffer, GMSTS_INDENT, 6 + player_index * 6);
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].arrows);
+
+    sprintf(text_buffer, "%05u" , status->player_stat[player_index].arrows);
     VDP_drawText(text_buffer, GMSTS_INDENT, 7 + player_index * 6);
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].balls);
+
+    sprintf(text_buffer, "%05u" , status->player_stat[player_index].balls);
     VDP_drawText(text_buffer, GMSTS_INDENT, 8 + player_index * 6);
-    sprintf(text_buffer, "%05d" , status->player_stat[player_index].lives);
+
+    sprintf(text_buffer, "%05u" , status->player_stat[player_index].lives);
     VDP_drawText(text_buffer, GMSTS_INDENT, 9 + player_index * 6);
+
 }
 
 void TST_erase_screen(){
