a283e5a4ded1ff887056575cbb5036445cd3df27
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2023-07-24 19:54:19 +0200

    Create levels 4 and 5

---
+++ res/level4.c
@@ -0,0 +1,76 @@
+#include "SOI.h"
+
+#define BOG (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_SLOT1)
+#define BOW (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_WHITE)
+
+static const Actor blp = {&PL_blue,
+       0,
+       0,
+    {BLOCK_TO_PX(17) - 8, BLOCK_TO_PX(2)},
+    {   0,    0},
+      0,
+      0,
+      0
+    };
+
+   static const Actor glp = {&PL_green,
+      0,
+      0,
+   {BLOCK_TO_PX(18) - 8, BLOCK_TO_PX(2)},
+   {   0,    0},
+      0,
+      0,
+      &blp
+   };
+
+static const Actor spinner = {&NST_spinner,
+   WALK_RIGHT,
+   0,
+   {BLOCK_TO_PX(9) - 8, BLOCK_TO_PX(3)},
+   {0, 0},
+      0,
+   0,
+   0,
+   };
+
+
+
+const Board level4 = {
+    {
+     SLC,   0,   0,   0,   0,   0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0, SLC,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     BOG, BOG, BOG,   0,   0,   0, BOG, BOG, BOG,   0, BOG, BOG, BOG,   0,   0,   0, BOG, BOG, BOG,
+     BOG,   0,   0,   0,   0, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG,   0,   0,   0,   0, BOG,
+       0, BOG,   0,   0, BOG, BOG, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOG, BOG,   0,   0, BOG,   0,
+     GLC, RDK,   0, BOG, BOG, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOG, BOG,   0, RDD, GLC,
+     BOG, BOG, BOG, BOG, BOW, BOW, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOW, BOW, BOG, BOG, BOG, BOG,
+     BOW, BOW, BOW, BOW, BOW, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOW, BOW, BOW, BOW, BOW,
+     BOG, BOG, BOG, BOG, BOG, BOG, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOG, BOG, BOG, BOG, BOG, BOG,
+     BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW,
+     BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG, BOG,
+     BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW,
+    },
+
+    {
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     GEM,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, GLD,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    },
+    {PAL_GR, PAL_R, PAL_BL},
+    MUS_HEAVY1,
+    0,
+    6000,
+    &spinner,
+    &glp
+};
---
+++ res/level5.c
@@ -0,0 +1,66 @@
+#include "SOI.h"
+
+#define BOW (NORMAL_BLOCK | SOLID | BLK_DOT | COL_WHITE)
+#define BRW (NORMAL_BLOCK | SOLID | BREAKABLE | BLK_DOT | COL_WHITE)
+
+static const Actor blp = {&PL_blue,
+       0,
+       0,
+    {BLOCK_TO_PX(7) - 8, BLOCK_TO_PX(12)},
+    {   0,    0},
+      0,
+      0,
+      0
+    };
+
+   static const Actor glp = {&PL_green,
+      0,
+      0,
+   {BLOCK_TO_PX(11) - 8, BLOCK_TO_PX(12)},
+   {   0,    0},
+      0,
+      0,
+      &blp
+   };
+
+
+
+const Board level5 = {
+    {
+     BOW, BOW, BOW, BOW, BOW, BOW, SLC,   0,   0, GLC,   0,   0, SLC, BOW, BOW, BOW, BOW, BOW, BOW,
+     BOW, BOW, BOW, BOW, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0, BOW, BOW, BOW, BOW, BOW,
+     BOW, BOW, BOW, BOW,   0,   0,   0,   0,   0, RDK,   0,   0,   0,   0,   0, BOW, BOW, BOW, BOW,
+     BOW, BOW, BOW,   0,   0, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW,   0,   0, BOW, BOW, BOW,
+     SLC,   0,   0,   0, BOW, BOW,   0,   0,   0,   0,   0,   0,   0, BOW, BOW,   0,   0,   0, SLC,
+       0,   0,   0,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0, BOW,   0,   0,   0,   0,
+     GLC,   0,   0,   0, BOW,   0,   0,   0,   0, GEM,   0,   0,   0,   0, BRW,   0,   0,   0, GLC,
+       0,   0,   0,   0, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0, BOW,   0,   0,   0,   0,
+     SLC,   0,   0,   0, BOW, BOW,   0,   0,   0,   0,   0,   0,   0, BOW, BOW,   0,   0,   0, SLC,
+     BOW, BOW, BOW,   0,   0, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW, BOW,   0,   0, BOW, BOW, BOW,
+     BOW, BOW, BOW, BOW,   0,   0,   0,   0,   0, RDD,   0,   0,   0,   0,   0, BOW, BOW, BOW, BOW,
+     BOW, BOW, BOW, BOW, BOW,   0,   0,   0,   0,   0,   0,   0,   0,   0, BOW, BOW, BOW, BOW, BOW,
+     BOW, BOW, BOW, BOW, BOW, BOW, SLC,   0,   0, GLC,   0,   0, SLC, BOW, BOW, BOW, BOW, BOW, BOW,
+    },
+
+    {
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0, GLC,   0,   0, C3H,   0,   0, GLC,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, ARR,   0,   0,   0,   0,
+       0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0, GLC,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    },
+    {PAL_GR, PAL_R, PAL_BL},
+    MUS_HEAVY1,
+    0,
+    6000,
+    0,
+    &glp
+};
