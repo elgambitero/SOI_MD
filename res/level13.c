ab764623230c3184bc4f143a46f61b768a03b480
Jaime Garcia Villena <garciavillena.jaime@gmail.com>
2023-07-24 23:43:16 +0200

    Write levels 13 and 14

---
+++ res/level13.c
@@ -0,0 +1,93 @@
+#include "SOI.h"
+
+#define BDW (NORMAL_BLOCK | SOLID | BLK_DONUT | COL_WHITE)
+#define BOW (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_WHITE)
+#define BFW (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_WHITE)
+#define BPB (NORMAL_BLOCK | SOLID | BREAKABLE | BLK_PLAYER)
+
+
+static const Actor blp = {&PL_blue,
+       0,
+       0,
+    {BLOCK_TO_PX(4) - 8, BLOCK_TO_PX(2)},
+    {   0,    0},
+      0,
+      0,
+      0
+    };
+
+   static const Actor glp = {&PL_green,
+      0,
+      0,
+   {BLOCK_TO_PX(16) - 8, BLOCK_TO_PX(2)},
+   {   0,    0},
+      0,
+      0,
+      &blp
+   };
+
+
+static const Actor robo0 = {
+    &NST_robo,
+    WALK_RIGHT,
+    0,
+    {BLOCK_TO_PX(3) - 8, BLOCK_TO_PX(10)},
+    {NASTIE_SPEED, 0},
+    0,
+    0,
+    0,
+};
+
+
+
+static const Actor robo1 = {
+    &NST_robo,
+    WALK_LEFT,
+    0,
+    {BLOCK_TO_PX(17) - 8, BLOCK_TO_PX(10)},
+    {-NASTIE_SPEED, 0},
+    0,
+    0,
+    &robo0,
+};
+
+// FIXME: Missing Moving blocks
+const Board level13 = {
+    {
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW,   0, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW,
+     SLC, SLC, GLC, GLC, ARR, BFW, SLC,   0,   0,   0,   0,   0, RDK, BFW, BAL, GLC, GLC, SLC, SLC,
+     SLC, SLC, BFW, BFW, BFW, BFW, BFW, BFW, BFW,   0, BFW, BFW, BFW, BFW, BFW, BFW, BFW, SLC, SLC,
+       0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0,
+       0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0,
+       0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0, BFW,   0,   0,   0,   0,   0,   0, BFW,   0,
+     BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW,   0, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+     GLC,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, RDD,
+    },
+
+    {
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    },
+    {PAL_GR, PAL_R, PAL_BL},
+    MUS_HEAVY3,
+    0,
+    6000,
+    &robo1,
+    &glp
+};
---
+++ res/level14.c
@@ -0,0 +1,137 @@
+#include "SOI.h"
+
+#define BDR (NORMAL_BLOCK | SOLID | BLK_DONUT | COL_SLOT1)
+#define BOR (NORMAL_BLOCK | SOLID | BLK_DOT   | COL_SLOT1)
+#define BFW (NORMAL_BLOCK | SOLID | BLK_FLAT  | COL_WHITE)
+#define BPB (NORMAL_BLOCK | SOLID | BREAKABLE | BLK_PLAYER)
+
+
+static const Actor blp = {&PL_blue,
+       0,
+       0,
+    {BLOCK_TO_PX(6) - 8, BLOCK_TO_PX(9)},
+    {   0,    0},
+      0,
+      0,
+      0
+    };
+
+   static const Actor glp = {&PL_green,
+      0,
+      0,
+   {BLOCK_TO_PX(7) - 8, BLOCK_TO_PX(9)},
+   {   0,    0},
+      0,
+      0,
+      &blp
+   };
+
+
+static const Actor beanie0 = {
+    &NST_beanie,
+    WALK_RIGHT,
+    0,
+    {BLOCK_TO_PX(5) - 8, BLOCK_TO_PX(6)},
+    {NASTIE_SPEED, 0},
+    0,
+    0,
+    0,
+};
+
+static const Actor wheell0 = {
+    &NST_whfastL,
+    NST_L_LEFT,
+    0,
+    {BLOCK_TO_PX(18) - 8, BLOCK_TO_PX(11)},
+    {-NASTIE_SPEED, 0},
+    0,
+    0,
+    &beanie0,
+};
+
+static const Actor wheelr1 = {
+    &NST_whfastR,
+    NST_R_RIGHT,
+    0,
+    {BLOCK_TO_PX(2) - 8, BLOCK_TO_PX(11)},
+    {NASTIE_SPEED, 0},
+    0,
+    0,
+    &whell0,
+};
+
+
+static const Actor wheell2 = {
+    &NST_whfastL,
+    NST_L_LEFT,
+    0,
+    {BLOCK_TO_PX(10) - 8, BLOCK_TO_PX(3)},
+    {-NASTIE_SPEED, 0},
+    0,
+    0,
+    &wheelr1,
+};
+
+static const Actor wheelr3 = {
+    &NST_whfastR,
+    NST_R_RIGHT,
+    0,
+    {BLOCK_TO_PX(10) - 8, BLOCK_TO_PX(12)},
+    {NASTIE_SPEED, 0},
+    0,
+    0,
+    &wheell2,
+};
+
+
+static const Actor spinner = {
+    &NST_spinner,
+    WALK_RIGHT,
+    0,
+    {BLOCK_TO_PX(10) - 8, BLOCK_TO_PX(8)},
+    {0, 0},
+    0,
+    0,
+    &wheelr3,
+};
+
+
+const Board level14 = {
+    {
+     BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,
+     BFW, BDR,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BDR, BFW,
+     BFW,   0, BDR,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BDR,   0, BFW,
+     BFW,   0,   0, BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,   0,   0, BFW,
+     BFW,   0,   0, BFW, QUE,   0,   0,   0,   0, GLC,   0,   0,   0,   0, QUE, BFW,   0,   0, BFW,
+     BFW,   0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0, BFW,
+     BFW,   0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0, BFW,
+     BFW,   0,   0, BFW,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BFW,   0,   0, BFW,
+     BFW,   0,   0, BFW, RDD,   0,   0,   0,   0, BPB,   0,   0,   0,   0, RDK, BFW,   0,   0, BFW,
+     BFW,   0,   0, BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,   0,   0, BFW,
+     BFW,   0, BDR,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BDR,   0, BFW,
+     BFW, BDR,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0, BDR, BFW,
+     BOR, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BFW, BOR,
+    },
+
+    {
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0, SLC,   0,   0,   0,   0,   0,   0,   0,   0,   0, ARR,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0, SHL,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+       0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+    },
+    {PAL_R, PAL_GR, PAL_BL},
+    MUS_HEAVY1,
+    0,
+    3000,
+    &spinner,
+    &glp
+};
